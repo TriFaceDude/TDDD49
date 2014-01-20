700c540859ce562d2c20ced13b4b686ef0a781ac
McFaceDude <samuel0808@gmail.com>
2014-01-20 16:13:57 +0100

    unit tests work

---
+++ TDDD49-Solution/UnitTest/PieceTest.cs
@@ -0,0 +1,180 @@
+﻿using TDDD49Lab;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+
+namespace UnitTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for PieceTest and is intended
+    ///to contain all PieceTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class PieceTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+
+        /// <summary>
+        ///A test for InverseSide
+        ///</summary>
+        [TestMethod()]
+        public void InverseSideTest_DtL()
+        {
+            Side side = Side.Light;
+            Side expected = Side.Dark;
+            Side actual;
+            actual = Piece.InverseSide(side);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for InverseSide
+        ///</summary>
+        [TestMethod()]
+        public void InverseSideTest_LtD()
+        {
+            Side side = Side.Dark;
+            Side expected = Side.Light;
+            Side actual;
+            actual = Piece.InverseSide(side);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for ValidMoves
+        ///</summary>
+        [TestMethod()]
+        public void ValidMovesTest_true00()
+        {
+            Piece target = PieceBuilder.MakeKnight(new Point(0,0), Side.Dark);
+            Board board = new Board();
+            target.UpdateValidMoveList(board);
+
+            List<Point> expected = new List<Point>() { new Point(1, 2), new Point(2, 1) };
+            List<Point> actual;
+            actual = target.ValidMoves;
+
+            bool result = true;
+
+            foreach (Point point in expected)
+            {
+                if (!actual.Contains(point))
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        ///A test for ValidMoves
+        ///</summary>
+        [TestMethod()]
+        public void ValidMovesTest_true22()
+        {
+            Piece target = PieceBuilder.MakeKnight(new Point(2, 2), Side.Dark);
+            Board board = new Board();
+            target.UpdateValidMoveList(board);
+
+            List<Point> expected = new List<Point>() { new Point(1, 0), new Point(0, 1), new Point(3, 0), new Point(0, 3), new Point(1, 4), new Point(4, 1), new Point(3, 4), new Point(4, 3) };
+            List<Point> actual;
+            actual = target.ValidMoves;
+
+            bool result = true;
+
+            foreach (Point point in expected)
+            {
+                if (!actual.Contains(point))
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        ///A test for ValidMoves
+        ///</summary>
+        [TestMethod()]
+        public void ValidMovesTest_false00()
+        {
+            Piece target = PieceBuilder.MakeKnight(new Point(0, 0), Side.Dark);
+            Board board = new Board();
+            target.UpdateValidMoveList(board);
+
+            List<Point> expected = new List<Point>() { new Point(3, 2), new Point(2, 1) };
+            List<Point> actual;
+            actual = target.ValidMoves;
+
+            bool result = true;
+
+            foreach (Point point in expected)
+            {
+                if (!actual.Contains(point))
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ TDDD49-Solution/UnitTest/PointTest.cs
@@ -0,0 +1,178 @@
+﻿using TDDD49Lab;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for PointTest and is intended
+    ///to contain all PointTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class PointTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+        /// <summary>
+        ///A test for Equals
+        ///</summary>
+        [TestMethod()]
+        public void EqualsTest_null()
+        {
+            Point target = new Point(1,1);
+            object obj = null;
+            bool expected = false;
+            bool actual;
+            actual = target.Equals(obj);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for Equals
+        ///</summary>
+        [TestMethod()]
+        public void EqualsTest_eq()
+        {
+            Point target = new Point(1,2);
+            object obj = new Point(1, 2);
+            bool expected = true;
+            bool actual;
+            actual = target.Equals(obj);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for Equals
+        ///</summary>
+        [TestMethod()]
+        public void EqualsTest_notEqY()
+        {
+            Point target = new Point(1, 4);
+            object obj = new Point(1, 2);
+            bool expected = false;
+            bool actual;
+            actual = target.Equals(obj);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for Equals
+        ///</summary>
+        [TestMethod()]
+        public void EqualsTest_notEqX()
+        {
+            Point target = new Point(3, 2);
+            object obj = new Point(1, 2);
+            bool expected = false;
+            bool actual;
+            actual = target.Equals(obj);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for op_Addition
+        ///</summary>
+        [TestMethod()]
+        public void op_AdditionTest_13_46()
+        {
+            Point a = new Point(1, 3);
+            Point b = new Point(4, 6);
+            Point expected = new Point(5, 9);
+            Point actual;
+            actual = (a + b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for op_Addition
+        ///</summary>
+        [TestMethod()]
+        public void op_AdditionTest()
+        {
+            Point a = new Point(1, 3);
+            Point b = new Point(-4, -6);
+            Point expected = new Point(-3, -3);
+            Point actual;
+            actual = (a + b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for op_Subtraction
+        ///</summary>
+        [TestMethod()]
+        public void op_SubtractionTest_13_46()
+        {
+            Point a = new Point(1,3);
+            Point b = new Point(4,6);
+            Point expected = new Point(-3, -3);
+            Point actual;
+            actual = (a - b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for op_Subtraction
+        ///</summary>
+        [TestMethod()]
+        public void op_SubtractionTest_31_46()
+        {
+            Point a = new Point(3, 1);
+            Point b = new Point(-4, -6);
+            Point expected = new Point(7, 7);
+            Point actual;
+            actual = (a - b);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
